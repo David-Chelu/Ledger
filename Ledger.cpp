c4fb1defed836e4f5403932cab7e4e7966507ed0
unknown <david.chelu@yahoo.com>
2024-05-23 23:06:04 +0300

    Added Ledger::SectionAttributes

--- Ledger.cpp
+++ Ledger.cpp
@@ -1,66 +1,25 @@
 #include "Code/TGL/TGL.h"
 #include "Code/File.h"
 #include "Code/Currency.h"
+#include "Code/Section.h"
 
 
 
 int main()
 {
-    TGL::Message("Dollar", Ledger::Currency{"dollar", "$", 0.2, false, Ledger::Currency::Position::Begin}.Transform(100));
-    TGL::Message("Dollar", Ledger::Currency{"dollar", "$", 0.2, true , Ledger::Currency::Position::Begin}.Transform(100));
-    TGL::Message("Dollar", Ledger::Currency{"dollar", "$", 0.2, false, Ledger::Currency::Position::End  }.Transform(100));
-    TGL::Message("Dollar", Ledger::Currency{"dollar", "$", 0.2, true , Ledger::Currency::Position::End  }.Transform(100));
+    Ledger::SectionAttributes().DisplayValues();
 
-return 0;
-
-    Ledger::File
-        file;
-
-
-
-//    Ledger::File{{{"Total", 500, "Car"},
-//                  {"2024.01.10", -200, "Tires"},
-//                  {"2024.02.10", 100, "Monthly Installment"}}}.WriteEntriesDirectly();
-
-//    file.WriteEntriesDirectly({{"A", 1, "a"},
-//                              {"B", 2, "b"},
-//                              {"C", 3, "c"}});
-    file.ReadEntriesDirectly();
-
-    file.DisplayEntries();
-
-
-
-    TGL::Message("", file.current.GetValues());
-
-    return 0;
-
-
+    MessageBox(NULL, "Diflüber", "Ich", 0);
 
     StartTGL();
 
 
 
-    LOGBRUSH
-        logBrush{BS_SOLID
-                ,RGB(0, 255, 0)
-                ,{}};
+    const COLORREF
+        background{TGL::Pixel(255, 255, 127)};
 
     bool
         update = true;
-
-    HBRUSH
-        brush = (HBRUSH)GetStockObject(BLACK_BRUSH);
-
-    HGDIOBJ
-        pen = ExtCreatePen(PS_GEOMETRIC
-                         | PS_INSIDEFRAME
-                         | PS_ENDCAP_ROUND
-                         | PS_JOIN_MITER
-                          ,5
-                          ,&logBrush
-                          ,0
-                          ,NULL);
 
     PAINTSTRUCT
         paint;
@@ -79,29 +38,30 @@ return 0;
     window.Create();
     window.Show();
 
+
+
+    const ColorWithSize_t
+        canvasData{background, window.current.width, window.current.height};
+
+    TGL::tglBitmap
+        blankCanvas{canvasData};
+
+
+
+    table.SetMode(TGL::VideoMode::Bitmap);
     table = window;
+    table = blankCanvas;
 
 
 
     table.Lock();
 
+    table.Display();
+
     LoopStart
     {
         if (update)
         {
-            BeginPaint(window.RequestHWND(), &paint);
-
-            SelectObject(table.RequestHDC(), brush);
-            SelectObject(table.RequestHDC(), pen);
-
-            Rectangle(table.RequestHDC(),
-                      0,
-                      0,
-                      window.xClient(),
-                      window.yClient());
-
-            EndPaint(window.RequestHWND(), &paint);
-
             update = false;
         }
 
